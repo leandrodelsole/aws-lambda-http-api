c1176f0e1d5740576670d56fb2a84fa248f7e6dd
Leandro Del Sole <leandro.sole@navita.com.br>
2019-07-07 17:13:21 -0300

    Utilizando custom runtime de java 11

--- src/main/java/br/com/lds/aws/lambda/http/api/controller/EchoHandler.java
+++ src/main/java/br/com/lds/aws/lambda/http/api/controller/EchoHandler.java
@@ -1,13 +1,22 @@
 package br.com.lds.aws.lambda.http.api.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-import br.com.lds.aws.lambda.http.api.dto.UserRequest;
+public class EchoHandler implements RequestStreamHandler {
 
-public class EchoHandler implements RequestHandler<UserRequest, UserRequest> {
 	@Override
-	public UserRequest handleRequest(UserRequest input, Context context) {
-		return input;
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		final LambdaLogger logger = context.getLogger();
+		for (String envVar : new String[]{"_HANDLER", "AWS_REGION", "AWS_EXECUTION_ENV", "AWS_LAMBDA_FUNCTION_NAME", "AWS_LAMBDA_FUNCTION_MEMORY_SIZE", "AWS_LAMBDA_FUNCTION_VERSION", "AWS_LAMBDA_LOG_GROUP_NAME", "AWS_LAMBDA_LOG_STREAM_NAME", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_SESSION_TOKEN", "LANG", "TZ", "LAMBDA_TASK_ROOT", "LAMBDA_RUNTIME_DIR", "PATH", "LD_LIBRARY_PATH", "NODE_PATH", "PYTHONPATH", "GEM_PATH", "AWS_LAMBDA_RUNTIME_API"}) {
+			logger.log("\n envVar " + envVar + " = " + System.getenv(envVar));
+		}
+
+		output.write("echoes".getBytes());
 	}
 }
--- src/main/java/br/com/lds/aws/lambda/http/api/controller/InsertUserHandler.java
+++ src/main/java/br/com/lds/aws/lambda/http/api/controller/InsertUserHandler.java
@@ -1,24 +1,34 @@
 package br.com.lds.aws.lambda.http.api.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.lds.aws.lambda.http.api.dto.UserRequest;
 import br.com.lds.aws.lambda.http.api.model.User;
 import br.com.lds.aws.lambda.http.api.service.UserService;
 
-public class InsertUserHandler implements RequestHandler<UserRequest, User> {
+public class InsertUserHandler implements RequestStreamHandler {
 
-	public User handleRequest(UserRequest userRequest, Context context) {
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
+		final ObjectMapper objectMapper = new ObjectMapper();
 		final LambdaLogger logger = context.getLogger();
+		final UserRequest userRequest = objectMapper.readValue(input, UserRequest.class);
+
 		logger.log("\nhandleRequest start " + userRequest + " remaining time " + context.getRemainingTimeInMillis());
 
 		final User user = new User(userRequest);
 		new UserService(context).create(user);
 
 		logger.log("\nhandleRequest end remaining time " + context.getRemainingTimeInMillis());
-		return user;
+
+		objectMapper.writeValue(output, user);
 	}
 }
