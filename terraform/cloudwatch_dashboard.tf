d6321a0e8c2f7e92e5401be798f4ca0022fcdeba
Leandro Del Sole <leandro.sole@navita.com.br>
2019-07-13 08:51:54 -0300

    Criando dashboards no cloud watch Removendo retorno da inserção, pra ficar comparável a versão Node

---
+++ terraform/cloudwatch_dashboard.tf
@@ -0,0 +1,161 @@
+#copied and adapted from https://github.com/FRosner/aws-lambda-monitoring-alerting-example/blob/master/terraform/cloudwatch_dashboard.tf
+locals {
+  dashboard-calculator-max-time-color   = "#1f77b4"
+  dashboard-calculator-avg-time-color   = "#9467bd"
+  dashboard-calculator-memory-color     = "#ff7f0e"
+  dashboard-calculator-error-color      = "#d62728"
+  dashboard-calculator-invocation-color = "#2ca02c"
+  project-name = "Java-Lambda-HTTP-API"
+}
+
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = "${local.project-name}"
+
+  dashboard_body = <<EOF
+{
+  "widgets": [
+    {
+      "type": "text",
+      "x": 0,
+      "y": 0,
+      "width": 24,
+      "height": 1,
+      "properties": {
+        "markdown": "# ${aws_lambda_function.insert-user.function_name}"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 1,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/Lambda", "Duration",
+            "FunctionName", "${aws_lambda_function.insert-user.function_name}",
+            "Resource", "${aws_lambda_function.insert-user.function_name}",
+            {
+              "stat": "Maximum",
+              "yAxis": "left",
+              "label": "Maximum Execution Time",
+              "color": "${local.dashboard-calculator-max-time-color}",
+              "period": 10
+            }
+          ],
+          [
+            "AWS/Lambda", "Duration",
+            "FunctionName", "${aws_lambda_function.insert-user.function_name}",
+            "Resource", "${aws_lambda_function.insert-user.function_name}",
+            {
+              "stat": "Average",
+              "yAxis": "left",
+              "label": "Average Execution Time",
+              "color": "${local.dashboard-calculator-avg-time-color}",
+              "period": 10
+            }
+          ]
+        ],
+        "view": "timeSeries",
+        "stacked": false,
+        "region": "${var.region}",
+        "yAxis": {
+          "left": {
+            "min": 0,
+            "max": ${aws_lambda_function.insert-user.timeout}000,
+            "label": "ms",
+            "showUnits": false
+          }
+        },
+        "title": "Execution Time",
+        "period": 300
+      }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 1,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${local.metrics-calculator-namespace}", "${local.metrics-calculator-memory-name}",
+            {
+              "stat": "Maximum",
+              "label": "Max Memory Used",
+              "color": "${local.dashboard-calculator-memory-color}",
+              "period": 10
+            }
+          ]
+        ],
+        "view": "timeSeries",
+        "stacked": false,
+        "region": "${var.region}",
+        "yAxis": {
+          "left": {
+            "min": 0,
+            "max": ${aws_lambda_function.insert-user.memory_size},
+            "showUnits": false,
+            "label": "MB"
+          }
+        },
+        "title": "Memory Consumption",
+        "period": 300
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 7,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/Lambda", "Errors",
+            "FunctionName", "${aws_lambda_function.insert-user.function_name}",
+            "Resource", "${aws_lambda_function.insert-user.function_name}",
+            {
+              "color": "${local.dashboard-calculator-error-color}",
+              "stat": "Sum",
+              "period": 10
+            }
+          ]
+        ],
+        "view": "timeSeries",
+        "stacked": false,
+        "region": "${var.region}",
+        "title": "Execution Errors"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 7,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/Lambda", "Invocations",
+            "FunctionName", "${aws_lambda_function.insert-user.function_name}",
+            "Resource", "${aws_lambda_function.insert-user.function_name}",
+            {
+              "color": "${local.dashboard-calculator-invocation-color}",
+              "stat": "Sum",
+              "period": 10
+            }
+          ]
+        ],
+        "view": "timeSeries",
+        "stacked": false,
+        "region": "${var.region}",
+        "title": "Invocations"
+      }
+    }
+  ]
+}
+EOF
+}
