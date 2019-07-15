5f6d2e7b5da198dc46b423cfb618efe3b754a967
Leandro Del Sole <leandro.sole@navita.com.br>
2019-07-15 17:14:08 -0300

    mantendo apenas a versão estável da GraalVM

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -17,18 +17,6 @@ rm -rf target/package
 mkdir -p target/server
 
 printf '\n\nbuild da GraalVM\n\n'
-#docker run --rm --name graal -v $(pwd):/working oracle/graalvm-ce:1.0.0-rc16 \
-#    /bin/bash -c "native-image --enable-url-protocols=http --enable-url-protocols=https \
-#                    -Djava.net.preferIPv4Stack=true \
-#                    -H:ReflectionConfigurationFiles=/working/runtime/reflect.json \
-#                    --delay-class-initialization-to-runtime=javax.net.ssl.HttpsURLConnection \
-#                    --no-fallback \
-#                    --no-server -jar /working/target/aws-lambda-http-api-1.0-SNAPSHOT.jar \
-#                    --enable-all-security-services \
-#                    ; \
-#                    cp aws-lambda-http-api-1.0-SNAPSHOT /working/target/server"
-#
-#                    --initialize-at-run-time=javax.net.ssl.HttpsURLConnection \
 docker run --rm --name graalvm-aws-lambda-http-api -v $(pwd):/working oracle/graalvm-ce:latest \
     /bin/bash -c "gu install native-image ; \
 					native-image --enable-url-protocols=http --enable-url-protocols=https \
